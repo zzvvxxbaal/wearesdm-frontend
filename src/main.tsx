42903c63d50b00266dc9e8e0bcb5d423946820db
zzvvxxbaal <w1tns61@gmail.com>
2026-08-09 16:59:25 +0900

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './app/AuthProvider'
+import { QueryProvider } from './app/QueryProvider'
+import { Router } from './app/Router'
+import { ErrorBoundary } from './components/ErrorBoundary'
+import './styles.css'
 
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryProvider>
+        <ErrorBoundary>
+          <AuthProvider>
+            <Router />
+          </AuthProvider>
+        </ErrorBoundary>
+      </QueryProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
---
+++ src/vite-env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string
+  readonly VITE_SUPABASE_PUBLISHABLE_KEY: string
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
